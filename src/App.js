13859aeca67fa37c2fa5ce47090bafddc9bc95d9
mikyGithub <54312715+mikyGithub@users.noreply.github.com>
2021-10-10 20:40:14 -0700

    `update`

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import "./style/App.css";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Header from "./components/Header";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY =  "contacts"
   const [contacts,setContacts] = useState([]);
   const addContactHandler = (contact)=>{
 
     // We use set state cause we can't update contacts directly
     setContacts([...contacts,contact])
   }
+  useEffect(()=>{
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    setContacts(retriveContacts);
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
+  },[contacts])
+  
 
   return (
     <div className="ui container">
